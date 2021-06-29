4a3c5bf07bb3e2c5c99ef452f1e72f1fc3179b6a
Dasha Yurchenko <dasha.yurchenko@inbox.lv>
2021-06-29 23:58:32 +0300

    Initial commit

---
+++ src/lv/sda/Main.java
@@ -0,0 +1,16 @@
+package lv.sda;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String firstNumber = "2";
+	String secondNumber = "97";
+
+	int number1 = Integer.parseInt(firstNumber);
+	int number2 = Integer.parseInt(secondNumber);
+
+        System.out.println(number1 + number2);
+
+
+    }
+}
